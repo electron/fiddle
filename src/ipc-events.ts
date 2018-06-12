1214963212ac54904802e0baa0144bf29376fbd5
Felix Rieseberg <felix@felixrieseberg.com>
2018-06-12 15:16:51 +0200

    :wrench: Make a central place for IPC calls

---
+++ src/ipc-events.ts
@@ -0,0 +1,9 @@
+export enum IpcEvents {
+  MAIN_WINDOW_READY_TO_SHOW = 'MAIN_WINDOW_READY_TO_SHOW'
+}
+
+export const ipcMainEvents = [
+  IpcEvents.MAIN_WINDOW_READY_TO_SHOW
+];
+
+export const ipcRendererEvents = [];
---
+++ src/renderer/ipc.ts
@@ -0,0 +1,36 @@
+import { EventEmitter } from 'events';
+import { ipcRenderer } from 'electron';
+import { ipcRendererEvents, IpcEvents } from '../ipc-events';
+
+/**
+ * The main purpose of this class is to be the central
+ * gathering place for IPC calls the main process sends
+ * or listens to.
+ *
+ * @class IpcManager
+ * @extends {EventEmitter}
+ */
+export class IpcRendererManager extends EventEmitter {
+  constructor() {
+    super();
+
+    ipcRendererEvents.forEach((name) => {
+      ipcRenderer.on(name, (...args) => this.emit(name, ...args));
+    });
+  }
+
+  /**
+   * Central method to send an IPC message to the main process.
+   * Currently very simple, but centralized here so that future
+   * refactors will be a lot easier.
+   *
+   * @param {IpcEvents} channel
+   * @param {...Array<any>} args
+   * @memberof IpcRendererManager
+   */
+  public send(channel: IpcEvents, ...args: Array<any>) {
+    ipcRenderer.send(channel, ...args);
+  }
+}
+
+export const ipcRendererManager = new IpcRendererManager();
