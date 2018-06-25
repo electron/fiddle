e95976006e45d754c7af53f572fa5f9b4ac62067
Felix Rieseberg <felix@felixrieseberg.com>
2018-06-24 18:45:06 -0700

    :heart: Feedback

--- tools/generateAssets.js
+++ tools/generateAssets.js
@@ -4,8 +4,4 @@ const { maybeFetchContributors } = require('./contributors');
 const { compileTypeScript } = require('./tsc');
 const { compileLess } = require('./lessc');
 
-module.exports = async () => {
-  await maybeFetchContributors();
-  await compileTypeScript();
-  await compileLess();
-}
+await Promise.all([maybeFetchContributors(), compileTypescript(), compileLess()]);
