abc60ea21f1eabfc320cf5557eb67dd7650f9619
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-22 01:45:30 +0800

    :microscope: Test get-title

---
+++ tests/utils/get-title-spec.ts
@@ -0,0 +1,39 @@
+import { getTitle, getName } from '../../src/utils/get-title';
+
+describe('get-title', () => {
+  const old = process.defaultApp;
+
+  describe('getTitle()', () => {
+    it('returns a solid default title', () => {
+      const result = getTitle({} as any);
+      expect(result).toBe('Electron Fiddle - Unsaved');
+    });
+
+    it('returns a title for a Gist', () => {
+      const result = getTitle({ gistId: '123' } as any);
+      expect(result).toBe('Electron Fiddle - gist.github.com/123');
+    });
+
+    it('returns a title for a local fiddle', () => {
+      const result = getTitle({ localPath: 'a/b/fiddle' } as any);
+      expect(result).toBe('Electron Fiddle - a/b/fiddle');
+    });
+
+    it('returns a title for a local & gist fiddle', () => {
+      const result = getTitle({ localPath: 'a/b/fiddle', gistId: '123' } as any);
+      expect(result).toBe('Electron Fiddle - gist.github.com/123 a/b/fiddle');
+    });
+  });
+
+  describe('getName()', () => {
+    it('returns a random name', async () => {
+      const result = await getName({} as any);
+      expect(result).toBeTruthy();
+    });
+
+    it('returns a name from the local path if saved', async () => {
+      const result = await getName({ localPath: 'a/b/myFiddle' } as any);
+      expect(result).toBe('myFiddle');
+    });
+  });
+});
