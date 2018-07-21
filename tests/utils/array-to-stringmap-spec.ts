ac5d34fe9273257328f57cbd96fe65f7af8fddbc
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-21 23:27:30 +0800

    :microscope: Test array-to-stringmap

---
+++ tests/utils/array-to-stringmap-spec.ts
@@ -0,0 +1,38 @@
+import { arrayToStringMap } from '../../src/utils/array-to-stringmap';
+import { GitHubVersion } from '../../src/interfaces';
+
+describe('array-to-stringmap', () => {
+  it('correctly turns an array of versions into a stringmap', () => {
+    const input: Array<Partial<GitHubVersion>> = [
+      {
+        name: 'Test Version 1',
+        tag_name: 'v1.0.0'
+      }, {
+        name: 'Test Version 2',
+        tag_name: 'v2.0.0'
+      }, {
+        name: 'Test Version 3',
+        tag_name: '3.0.0'
+      }
+    ];
+    const output = arrayToStringMap(input as any);
+
+    expect(output).toEqual({
+      '1.0.0': {
+        name: 'Test Version 1',
+        tag_name: 'v1.0.0',
+        state: 'unknown'
+      },
+      '2.0.0': {
+        name: 'Test Version 2',
+        tag_name: 'v2.0.0',
+        state: 'unknown'
+      },
+      '3.0.0': {
+        name: 'Test Version 3',
+        tag_name: '3.0.0',
+        state: 'unknown'
+      },
+    });
+  });
+});
