001f377e29d3eae3d7b8606b417840709519ad84
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-21 23:37:33 +0800

    :microscope: Test devmode

---
+++ tests/utils/devmode-spec.ts
@@ -0,0 +1,27 @@
+import { isDevMode } from '../../src/utils/devmode';
+
+describe('devMode', () => {
+  const old = process.defaultApp;
+
+  afterEach(() => {
+    Object.defineProperty(process, 'defaultApp', { value: old });
+  });
+
+  it('correctly returns true if defaultApp', () => {
+    Object.defineProperty(process, 'defaultApp', {
+      value: true,
+      writable: true
+    });
+
+    expect(isDevMode()).toBe(true);
+  });
+
+  it('correctly returns false if not defaultApp', () => {
+    Object.defineProperty(process, 'defaultApp', {
+      value: undefined,
+      writable: true
+    });
+
+    expect(isDevMode()).toBe(false);
+  });
+});
