d3651ef4caf86899bcf01dc26e038a4e88a92dff
Felix Rieseberg <felix@felixrieseberg.com>
2018-07-30 09:11:41 -0700

    :microscope: Test GitHubSettings component

---
+++ tests/renderer/components/settings-github-spec.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import { GitHubSettings } from '../../../src/renderer/components/settings-github';
+
+describe('GitHubSettings component', () => {
+  beforeEach(() => {
+    this.store = {
+      signOutGitHub: jest.fn()
+    };
+  });
+
+  it('renders when not signed in', () => {
+    const wrapper = shallow(
+      <GitHubSettings appState={this.store} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders when signed in', () => {
+    this.store.gitHubToken = '123';
+    this.store.gitHubLogin = 'Test User';
+
+    const wrapper = shallow(
+      <GitHubSettings appState={this.store} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('handles a click on the signout button', () => {
+    this.store.gitHubToken = '123';
+    this.store.gitHubLogin = 'Test User';
+
+    const wrapper = shallow(
+      <GitHubSettings appState={this.store} />
+    );
+
+    wrapper.find('button').simulate('click');
+    expect(this.store.signOutGitHub).toHaveBeenCalled();
+  });
+});
