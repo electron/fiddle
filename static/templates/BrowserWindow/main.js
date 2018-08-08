558431a85c5a01c555ef9a01dc46c2886f0daef6
Felix Rieseberg <felix@felixrieseberg.com>
2018-08-07 19:22:47 -0700

    :school: Template for BrowserWindow

--- static/templates/BrowserWindow/main.js
+++ static/templates/BrowserWindow/main.js
@@ -2,23 +2,52 @@
 // in Electron. A BrowserWindow is, like the name suggests, a window.
 //
 // For more info, see:
-// https://github.com/electron/electron/blob/master/docs/api/browser-window.md
+// https://electronjs.org/docs/api/browser-window
 
 const { app, BrowserWindow } = require('electron')
+
+const windows = []
 
 app.on('ready', () => {
   // BrowserWindows can be created in plenty of shapes, sizes, and forms.
   // Check out the editor's auto-completion for all the configuration
   // options available in the current version.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+  //
+  // Let's make a few windows!
 
-  win.on('closed', () => {
-    win = null
-  })
+  // A window that's only half visible
+  windows.push(
+    new BrowserWindow({
+      opacity: 0.5,
+      x: 200,
+      y: 200
+    })
+  )
 
-  // Load a remote URL
-  win.loadURL('https://github.com')
+  // A transparent window
+  windows.push(
+    new BrowserWindow({
+      transparent: true,
+      x: 300,
+      y: 300
+    })
+  )
+
+  // A window that's fixed and always on top
+  windows.push(
+    new BrowserWindow({
+      movable: false,
+      resizable: false,
+      alwaysOnTop: true,
+      maximizable: false,
+      minimizable: false,
+      x: 600,
+      y: 600
+    })
+  )
+
+  windows.forEach((window) => {
+    // Load our index.html
+    window.loadFile('index.html')
+  })
 })
