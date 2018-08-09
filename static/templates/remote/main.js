5b0d17c88a1986875e058c1e3306632a95c59e88
Felix Rieseberg <felix@felixrieseberg.com>
2018-08-08 17:44:36 -0700

    :school: Template for remote

---
+++ static/templates/remote/main.js
@@ -0,0 +1,14 @@
+// Use main process modules from the renderer process.
+//
+// For more info, see:
+// https://electronjs.org/docs/api/remote
+
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow = null
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ height: 600, width: 600 })
+
+  mainWindow.loadFile('index.html')
+})
