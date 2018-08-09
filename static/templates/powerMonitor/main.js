685637e6c9ee2ae889a7d44a15791e9e63305885
Felix Rieseberg <felix@felixrieseberg.com>
2018-08-08 17:44:53 -0700

    :school: Template for powerMonitor

---
+++ static/templates/powerMonitor/main.js
@@ -0,0 +1,24 @@
+// Monitor power state changes.
+//
+// For more info, see:
+// https://electronjs.org/docs/api/power-monitor
+
+const { app, powerMonitor } = require('electron')
+
+app.on('ready', () => {
+  powerMonitor.on('suspend', () => {
+    console.log('The system is going to sleep')
+  })
+
+  powerMonitor.on('resume', () => {
+    console.log('The system is waking up')
+  })
+
+  powerMonitor.on('on-ac', () => {
+    console.log(`We're on AC power`)
+  })
+
+  powerMonitor.on('on-battery', () => {
+    console.log(`We're on AC power`)
+  })
+})
