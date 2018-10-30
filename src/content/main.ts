57ce8b0b796d96ba486ba8158a7f7905258f707f
Shelley Vohr <codebytere@github.com>
2018-10-30 16:55:08 -0700

    chore: remove unused requires in main (#109)

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,7 +1,5 @@
 export const main = `// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
