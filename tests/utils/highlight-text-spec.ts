e519ce2f13a514edefe173a713972a31b85b4ef9
Felix Rieseberg <felix@felixrieseberg.com>
2019-01-02 21:54:09 +0800

    test: Add highlight spec

--- tests/utils/highlight-text-spec.ts
+++ tests/utils/highlight-text-spec.ts
@@ -5,4 +5,14 @@ describe('highlightText', () => {
     const input = highlightText('test-string', 'test');
     expect(input).toHaveLength(2);
   });
+
+  it('highlights text (multiple matches)', () => {
+    const input = highlightText('test-string-test', 'test');
+    expect(input).toHaveLength(3);
+  });
+
+  it('does not highlight text', () => {
+    const input = highlightText('test-string', 'abc');
+    expect(input).toHaveLength(1);
+  });
 });
