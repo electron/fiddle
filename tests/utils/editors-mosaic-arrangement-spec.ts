46deeb857e6b7fb9c7a83fc644ed543ab670b4b9
Felix Rieseberg <felix@felixrieseberg.com>
2019-03-31 13:35:34 -0700

    test: Test editors-mosaic-arrangement

---
+++ tests/utils/editors-mosaic-arrangement-spec.ts
@@ -0,0 +1,59 @@
+import { EditorId } from '../../src/interfaces';
+import { DEFAULT_MOSAIC_ARRANGEMENT } from '../../src/renderer/state';
+import { createMosaicArrangement, getVisibleEditors } from '../../src/utils/editors-mosaic-arrangement';
+
+describe('Mosaic Arrangement Utilities', () => {
+  describe('createMosaicArrangement()', () => {
+    it('creates the correct arrangement for one visible panel', () => {
+      const result = createMosaicArrangement([ EditorId.main ]);
+
+      expect(result).toEqual(EditorId.main);
+    });
+
+    it('creates the correct arrangement for two visible panels', () => {
+      const result = createMosaicArrangement([ EditorId.main, EditorId.renderer ]);
+
+      expect(result).toEqual({
+        direction: 'column',
+        first: EditorId.main,
+        second: EditorId.renderer
+      });
+    });
+
+    it('creates the correct arrangement for three visible panels', () => {
+      const result = createMosaicArrangement([ EditorId.main, EditorId.renderer, EditorId.html ]);
+
+      expect(result).toEqual(DEFAULT_MOSAIC_ARRANGEMENT);
+    });
+  });
+
+  describe('getVisibleEditors()', () => {
+    it('returns the correct array for no panels', () => {
+      const result = getVisibleEditors(null);
+
+      expect(result).toEqual([]);
+    });
+
+    it('returns the correct array for one visible panel', () => {
+      const result = getVisibleEditors(EditorId.main);
+
+      expect(result).toEqual([ EditorId.main ]);
+    });
+
+    it('returns the correct array for two visible panels', () => {
+      const result = getVisibleEditors({
+        direction: 'column',
+        first: EditorId.main,
+        second: EditorId.renderer
+      });
+
+      expect(result).toEqual([ EditorId.main, EditorId.renderer ]);
+    });
+
+    it('returns the correct array for three visible panels', () => {
+      const result = getVisibleEditors(DEFAULT_MOSAIC_ARRANGEMENT);
+
+      expect(result).toEqual([ EditorId.main, EditorId.renderer, EditorId.html ]);
+    });
+  });
+});
