90c198d48728612760a183867462e455c971f73d
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-03 16:33:21 -0700

    feat: A generic "more information" component

---
+++ tests/renderer/components/show-me/more-information-spec.tsx
@@ -0,0 +1,18 @@
+import * as electron from 'electron';
+import { mount, shallow } from 'enzyme';
+import * as React from 'react';
+
+import { renderMoreDocumentation } from '../../../../src/renderer/components/show-me/more-information';
+
+describe('renderMoreDocumentation()', () => {
+  it('renders', () => {
+    const wrapper = mount(renderMoreDocumentation());
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('a#open-url').simulate('click');
+    expect(electron.shell.openExternal).toHaveBeenCalledTimes(1);
+
+    wrapper.find('a#open-github').simulate('click');
+    expect(electron.shell.openExternal).toHaveBeenCalledTimes(2);
+  });
+});
