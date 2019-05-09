5d572f451cf1f17605966e73080299787db6336e
Ankur Jyoti Phukan <leocreed1994@gmail.com>
2019-05-09 11:54:52 +0530

    Added webPreference

--- static/show-me/desktopcapturer/main.js
+++ static/show-me/desktopcapturer/main.js
@@ -9,7 +9,10 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow = null
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ height: 600, width: 600 })
+  mainWindow = new BrowserWindow({ height: 600, width: 600,
+    webPreferences: {
+      nodeIntegration: true
+    } })
 
   mainWindow.loadFile('index.html')
 })
