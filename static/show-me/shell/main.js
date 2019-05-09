2b34ef5e67adb10ed5e20d1688202868daec1491
Ankur Jyoti Phukan <leocreed1994@gmail.com>
2019-05-09 12:23:08 +0530

    Update main.js

--- static/show-me/shell/main.js
+++ static/show-me/shell/main.js
@@ -9,7 +9,7 @@ let mainWindow = null
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 600,
     height: 600,
     webPreferences: {
       nodeIntegration: true
