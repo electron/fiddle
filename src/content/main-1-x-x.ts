76702e6dd24e1c9e0202c5ea2718c91b98316cbe
Charles Kerr <ckerr@github.com>
2020-03-26 18:23:54 -0500

    Update src/content/main-1-x-x.ts
    Co-Authored-By: Mark Lee <malept@users.noreply.github.com>

--- src/content/main-1-x-x.ts
+++ src/content/main-1-x-x.ts
@@ -21,7 +21,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
