41931d6bcaf0706a51db013ef17f70fd24dc648e
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-04-29 11:09:29 -0700

    fix: styled reset button adding React Fragment and MenuDivider

--- src/renderer/components/commands-editors.tsx
+++ src/renderer/components/commands-editors.tsx
@@ -1,4 +1,4 @@
-import { Button, Menu, MenuItem, Popover, Position } from '@blueprintjs/core';
+import { Button, Menu, MenuDivider, MenuItem, Popover, Position } from '@blueprintjs/core';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 
@@ -83,11 +83,15 @@ export class EditorDropdown extends React.Component<EditorDropdownProps, EditorD
     }
 
     result.push(
-      <Button
-        icon='grid-view'
-        text='Reset Layout'
-        onClick={appState.resetEditorLayout}
-      />
+      <React.Fragment key={'fragment-reset-layout'}>
+      <MenuDivider />
+        <MenuItem
+          icon='grid-view'
+          key='reset-layout'
+          text='Reset Layout'
+          onClick={appState.resetEditorLayout}
+        />
+      </React.Fragment>
     );
 
     return result;
