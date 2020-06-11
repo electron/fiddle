a0e59ad2881304bceb392821a233ba20c7c87541
Benjamin Sova <65474198+BenSova@users.noreply.github.com>
2020-06-11 10:48:38 -0600

    docs: fix explanation of darwin auto-quit prevention (#410)

--- src/content/main.ts
+++ src/content/main.ts
@@ -23,10 +23,10 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
