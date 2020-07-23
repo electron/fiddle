93e02ca44c8576c3f96e32b00d4d746291f11204
Erick Zhao <ezhao@slack-corp.com>
2020-07-23 10:48:37 -0700

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,10 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,5 @@ module.exports = {
   semi: true,
   trailingComma: "all",
   singleQuote: true,
-  printWidth: 80,
   tabWidth: 2
 };
