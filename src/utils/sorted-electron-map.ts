81b27ebee36a620c7d6057c7f4b36dd3956e7355
Shelley Vohr <shelley.vohr@gmail.com>
2020-09-17 23:02:08 -0600

    fix: version prerelease sorting

--- src/utils/sorted-electron-map.ts
+++ src/utils/sorted-electron-map.ts
@@ -20,12 +20,28 @@ export function sortedElectronMap<T>(
         return a.localeCompare(b);
       }
 
-      if (!semver.valid(a)) {
-        return -1;
-      }
+      if (!semver.valid(a)) return -1;
+      if (!semver.valid(b)) return 1;
 
-      if (!semver.valid(b)) {
-        return 1;
+      // Handle prerelease tag sorting - semver does not handle this.
+      // See https://www.npmjs.com/package/semver#prerelease-tags for more.
+      const bothPre = semver.prerelease(a) && semver.prerelease(b);
+      const sameCoerced =
+        semver.valid(semver.coerce(a)) === semver.valid(semver.coerce(b));
+      if (bothPre && sameCoerced) {
+        const [tagA, valueA] = semver.prerelease(a)!;
+        const [tagB, valueB] = semver.prerelease(b)!;
+
+        if (tagA === 'beta' && tagA === tagB) {
+          // Both are betas - compare beta number.
+          return valueA > valueB ? -1 : 1;
+        } else if (tagA === 'nightly' && tagA === tagB) {
+          // Both are nightlies - compare nightly dates strings.
+          return -`${valueA}`.localeCompare(`${valueB}`);
+        } else {
+          // beta > nightly for the same major version.
+          return tagA === 'beta' ? -1 : 1;
+        }
       }
 
       return semver.gt(a, b, true) ? -1 : 1;
