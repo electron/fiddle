2a0160f4f94935e47595fd347b8307fb4ff90977
Shelley Vohr <shelley.vohr@gmail.com>
2020-09-19 20:59:04 -0600

    fix: version prerelease sorting (#485)

--- tests/utils/sorted-electron-map-spec.ts
+++ tests/utils/sorted-electron-map-spec.ts
@@ -1,6 +1,6 @@
 import { sortedElectronMap } from '../../src/utils/sorted-electron-map';
 
-describe('sorted-eletron-map', () => {
+describe('sorted-electron-map', () => {
   it('sorts a record of electron versions', () => {
     const map: any = {
       '1.0.0': {
@@ -13,11 +13,58 @@ describe('sorted-eletron-map', () => {
         version: 'v2.0.0',
       },
     };
-    const result = sortedElectronMap(map, (_k, e) => e);
 
-    expect(result[0]).toEqual({ version: 'v3.0.0' });
-    expect(result[1]).toEqual({ version: 'v2.0.0' });
-    expect(result[2]).toEqual({ version: 'v1.0.0' });
+    const result = sortedElectronMap(map, (_k, e) => e);
+    expect(result).toStrictEqual([
+      { version: 'v3.0.0' },
+      { version: 'v2.0.0' },
+      { version: 'v1.0.0' },
+    ]);
+  });
+
+  it('sorts nightly and beta versions correctly', () => {
+    const map: any = {
+      '2.0.0-nightly.20200101': {
+        version: 'v2.0.0-nightly.20200101',
+      },
+      '2.0.0-nightly.20200102': {
+        version: 'v2.0.0-nightly.20200102',
+      },
+      '2.0.0': {
+        version: 'v2.0.0',
+      },
+      '3.0.0-nightly.20200105': {
+        version: 'v3.0.0-nightly.20200105',
+      },
+      '3.0.0': {
+        version: 'v3.0.0',
+      },
+      '3.0.0-beta.1': {
+        version: 'v3.0.0-beta.1',
+      },
+      '2.0.0-beta.1': {
+        version: 'v2.0.0-beta.1',
+      },
+      '2.0.0-beta.2': {
+        version: 'v2.0.0-beta.2',
+      },
+      '2.0.0-beta.3': {
+        version: 'v2.0.0-beta.3',
+      },
+    };
+
+    const result = sortedElectronMap(map, (_k, e) => e);
+    expect(result).toStrictEqual([
+      { version: 'v3.0.0' },
+      { version: 'v3.0.0-beta.1' },
+      { version: 'v3.0.0-nightly.20200105' },
+      { version: 'v2.0.0' },
+      { version: 'v2.0.0-beta.3' },
+      { version: 'v2.0.0-beta.2' },
+      { version: 'v2.0.0-beta.1' },
+      { version: 'v2.0.0-nightly.20200102' },
+      { version: 'v2.0.0-nightly.20200101' },
+    ]);
   });
 
   it('handles invalid versions', () => {
@@ -35,11 +82,13 @@ describe('sorted-eletron-map', () => {
         version: 'garbage',
       },
     };
-    const result = sortedElectronMap(map, (_k, e) => e);
 
-    expect(result[0]).toEqual({ version: 'garbage' });
-    expect(result[1]).toEqual({ version: 'moreGarbage' });
-    expect(result[2]).toEqual({ version: 'v3.0.0' });
-    expect(result[3]).toEqual({ version: 'v1.0.0' });
+    const result = sortedElectronMap(map, (_k, e) => e);
+    expect(result).toStrictEqual([
+      { version: 'garbage' },
+      { version: 'moreGarbage' },
+      { version: 'v3.0.0' },
+      { version: 'v1.0.0' },
+    ]);
   });
 });
