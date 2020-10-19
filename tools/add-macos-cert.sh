b015113c84a0762d9bf55cb8bb3185ad7258088a
Felix Rieseberg <felix@felixrieseberg.com>
2020-10-19 10:21:50 -0700

    build: Correct secret names

--- tools/add-macos-cert.sh
+++ tools/add-macos-cert.sh
@@ -4,7 +4,7 @@ KEY_CHAIN=build.keychain
 MACOS_CERT_P12_FILE=certificate.p12
 
 # Recreate the certificate from the secure environment variable
-echo $MACOS_CERT_P12 | base64 --decode > $MACOS_CERT_P12_FILE
+echo $MACOS_CODESIGN_P12 | base64 --decode > $MACOS_CERT_P12_FILE
 
 #create a keychain
 security create-keychain -p actions $KEY_CHAIN
@@ -15,7 +15,7 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERT_PASSWORD -T /usr/bin/codesign;
+security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CODESIGN_PASSWORD -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
