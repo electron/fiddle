1892d765d65a531a90c5c6e8e0cddc5f328b2340
Erick Zhao <erick@hotmail.ca>
2021-03-09 16:30:49 -0800

    test: add spec for preload

---
+++ tests/preload/preload-spec.ts
@@ -0,0 +1,51 @@
+/**
+ * @jest-environment node
+ */
+import * as electron from 'electron';
+import { IpcEvents } from '../../src/ipc-events';
+import { setupGlobalWindow, setupPaths } from '../../src/preload/preload';
+
+describe('preload', () => {
+  // We instantiate this in `tests/setup.js` for the main and
+  // renderer processes, but we don't need these mocks since
+  // these things are being instantiated within the preload
+  beforeEach(() => {
+    delete (window as any).ElectronFiddle;
+  });
+
+  describe('setupGlobalWindow()', () => {
+    it('sets up a window.ElectronFiddle object', () => {
+      setupGlobalWindow();
+
+      expect((window as any).ElectronFiddle).toMatchObject({
+        app: null,
+        editors: {
+          main: null,
+          preload: null,
+          renderer: null,
+          html: null,
+        },
+      });
+    });
+  });
+
+  describe('setupPaths()', () => {
+    beforeEach(() => {
+      setupGlobalWindow();
+    });
+
+    it('sets app paths', async () => {
+      const obj = {
+        appPath: '/fake/path',
+      };
+      (electron.ipcRenderer.invoke as jest.Mock).mockResolvedValue(obj);
+
+      await setupPaths();
+
+      expect(electron.ipcRenderer.invoke).toHaveBeenCalledWith(
+        IpcEvents.SET_APPDATA_DIR,
+      );
+      expect((window as any).ElectronFiddle.appPaths).toBe(obj);
+    });
+  });
+});
