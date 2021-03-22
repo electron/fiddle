dfb708817e72f9f6d0d929f655de421ab140e44b
Charles Kerr <charles@charleskerr.com>
2021-03-22 13:56:12 -0500

    refactor: restore the "describe()" in fancy-import-spec

--- tests/utils/fancy-import-spec.ts
+++ tests/utils/fancy-import-spec.ts
@@ -1,22 +1,24 @@
 import { fancyImport } from '../../src/utils/import';
 
-it('imports es6 modules', async () => {
-  const name = 'fs';
-  const required = require(name);
-  const imported: any = await fancyImport(name);
+describe('fancyImport()', () => {
+  it('imports es6 modules', async () => {
+    const name = 'fs';
+    const required = require(name);
+    const imported: any = await fancyImport(name);
 
-  // https://www.typescriptlang.org/tsconfig#esModuleInterop
-  // can inject a 'default' for compatibility, so the imports may
-  // differ from require in that respect. Remove that from the equation before comparing.
-  const importedMinusDefault = { ...imported };
-  delete importedMinusDefault.default;
+    // https://www.typescriptlang.org/tsconfig#esModuleInterop
+    // can inject a 'default' for compatibility, so the imports may
+    // differ from require in that respect. Remove that from the equation before comparing.
+    const importedMinusDefault = { ...imported };
+    delete importedMinusDefault.default;
 
-  expect(required).toEqual(importedMinusDefault);
-});
+    expect(required).toEqual(importedMinusDefault);
+  });
 
-it('imports cjs single-function modules', async () => {
-  const name = 'decompress';
-  const required = require(name);
-  const imported: any = await fancyImport(name);
-  expect(required).toEqual(imported.default);
+  it('imports cjs single-function modules', async () => {
+    const name = 'decompress';
+    const required = require(name);
+    const imported: any = await fancyImport(name);
+    expect(required).toEqual(imported.default);
+  });
 });
