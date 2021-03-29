126c0aa2435f4ce2c98a8036427a1074fc28521a
Charles Kerr <charles@charleskerr.com>
2021-03-29 09:16:32 -0500

    fix: inconsistent sorting of editor windows (#624)
    * fix: inconsistent sorting of editor windows
    
    On startup the editor windows are sorted one way; then if you hit
    'New Fiddle' the editors will change order.
    
    The root cause is that constants.ts's DEFAULT_MOSAIC_ARRANGEMENT and the
    'order' array in app.tsx's replaceFiddle() do not have the same order.
    This PR makes one derive from the other so that the two always match.
    
    * test: sync tests to SORTED_EDITORS order

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -18,7 +18,7 @@ import { getEditorValue } from '../utils/editor-value';
 import { getPackageJson, PackageJsonOptions } from '../utils/get-package';
 import { getTitle } from '../utils/get-title';
 import { isEditorBackup } from '../utils/type-checks';
-import { EMPTY_EDITOR_CONTENT } from './constants';
+import { EMPTY_EDITOR_CONTENT, SORTED_EDITORS } from './constants';
 import { FileManager } from './file-manager';
 import { RemoteLoader } from './remote-loader';
 import { Runner } from './runner';
@@ -64,33 +64,16 @@ export class App {
       }
     }
 
-    // display all editors that have content
-    const visibleEditors = [];
+    // if the gist content is empty or matches the empty file output, don't show it
+    const EMPTIES = Object.values(EMPTY_EDITOR_CONTENT);
+    const shouldShowContent = (content?: string) =>
+      content && content.length > 0 && !EMPTIES.includes(content);
 
-    // ensure consistent sorting of mosaics
-    const sortedEditors = Object.keys(editorValues).sort((a, b) => {
-      const order: Array<string> = [
-        'main',
-        'renderer',
-        'html',
-        'preload',
-        'css',
-      ];
-
-      return order.indexOf(a) - order.indexOf(b);
-    });
-
-    for (const id of sortedEditors) {
-      const content = editorValues[id];
-
-      // if the gist content is empty or matches the empty file output, don't show it
-      if (
-        content.length > 0 &&
-        !Object.values(EMPTY_EDITOR_CONTENT).includes(content)
-      ) {
-        visibleEditors.push(id as EditorId);
-      }
-    }
+    // sort and display all editors that have content
+    const visibleEditors: EditorId[] = Object.entries(editorValues)
+      .filter(([_id, content]) => shouldShowContent(content))
+      .map(([id]) => id as EditorId)
+      .sort((a, b) => SORTED_EDITORS.indexOf(a) - SORTED_EDITORS.indexOf(b));
 
     this.state.gistId = gistId || '';
     this.state.localPath = filePath;
--- src/shared-constants.ts
+++ src/shared-constants.ts
@@ -1,3 +1,5 @@
+import { EditorId } from './interfaces';
+
 export const INDEX_HTML_NAME = 'index.html';
 export const MAIN_JS_NAME = 'main.js';
 export const RENDERER_JS_NAME = 'renderer.js';
@@ -6,9 +8,9 @@ export const STYLES_CSS_NAME = 'styles.css';
 export const PACKAGE_NAME = 'package.json';
 
 export const FILENAME_KEYS = Object.freeze({
-  [INDEX_HTML_NAME]: 'html',
-  [MAIN_JS_NAME]: 'main',
-  [PRELOAD_JS_NAME]: 'preload',
-  [RENDERER_JS_NAME]: 'renderer',
-  [STYLES_CSS_NAME]: 'css',
+  [INDEX_HTML_NAME]: EditorId.html,
+  [MAIN_JS_NAME]: EditorId.main,
+  [PRELOAD_JS_NAME]: EditorId.preload,
+  [RENDERER_JS_NAME]: EditorId.renderer,
+  [STYLES_CSS_NAME]: EditorId.css,
 });
