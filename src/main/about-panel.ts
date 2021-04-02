6ae4ba62067d73924283365de37dee35ff02c9cd
Charles Kerr <charles@charleskerr.com>
2021-04-01 23:13:06 -0500

    chore: fix unused-import warning

--- src/main/about-panel.ts
+++ src/main/about-panel.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { AboutPanelOptionsOptions, app } from 'electron';
+import { app } from 'electron';
 
 /**
  * Sets Fiddle's About panel options on Linux and macOS
