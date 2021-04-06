164463c5a62172e00badf94a14910b7383a7c4ae
Charles Kerr <charles@charleskerr.com>
2021-04-06 12:10:30 -0500

    test: add MockVersions object to get new state every test

--- tests/mocks/electron-versions.ts
+++ tests/mocks/electron-versions.ts
@@ -4,32 +4,32 @@ import {
   VersionState,
 } from '../../src/interfaces';
 
-export const mockVersions: Record<string, RunnableVersion> = {
-  '2.0.3': {
-    source: VersionSource.remote,
-    state: VersionState.ready,
-    version: '2.0.3',
-  },
-  '2.0.2': {
-    source: VersionSource.remote,
-    state: VersionState.ready,
-    version: '2.0.2',
-  },
-  '2.0.1': {
-    source: VersionSource.remote,
-    state: VersionState.ready,
-    version: '2.0.1',
-  },
-  '2.0.0': {
-    source: VersionSource.remote,
-    state: VersionState.ready,
-    version: '2.0.0',
-  },
-  '1.8.7': {
-    source: VersionSource.remote,
-    state: VersionState.ready,
-    version: '1.8.7',
-  },
-};
+// prefer using this, since making new instances prevents state
+// from bleeding between tests.
+export class MockVersions {
+  public readonly array: RunnableVersion[];
+  public readonly mockVersions: Record<string, RunnableVersion>;
+  public readonly mockVersionsArray: RunnableVersion[];
+  public readonly object: Record<string, RunnableVersion>;
 
-export const mockVersionsArray = Object.values(mockVersions);
+  constructor() {
+    const versions = ['2.0.3', '2.0.2', '2.0.1', '2.0.0', '1.8.7'];
+
+    this.array = versions.map((version) => ({
+      source: VersionSource.remote,
+      state: VersionState.ready,
+      version,
+    }));
+
+    this.object = {};
+    for (const ver of this.array) {
+      this.object[ver.version] = ver;
+    }
+
+    this.mockVersions = this.object;
+    this.mockVersionsArray = this.array;
+  }
+}
+
+// export static instances for the tests that still use them
+export const { mockVersions, mockVersionsArray } = new MockVersions();
