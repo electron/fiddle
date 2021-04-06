b26cd7ca88f641dbb08325bfb361d3609a180729
Charles Kerr <charles@charleskerr.com>
2021-04-06 12:27:23 -0500

    refactor: remove temporary shims from MockVersions

--- tests/mocks/electron-versions.ts
+++ tests/mocks/electron-versions.ts
@@ -4,32 +4,25 @@ import {
   VersionState,
 } from '../../src/interfaces';
 
-// prefer using this, since making new instances prevents state
-// from bleeding between tests.
 export class MockVersions {
-  public readonly array: RunnableVersion[];
   public readonly mockVersions: Record<string, RunnableVersion>;
   public readonly mockVersionsArray: RunnableVersion[];
-  public readonly object: Record<string, RunnableVersion>;
 
   constructor() {
     const versions = ['2.0.3', '2.0.2', '2.0.1', '2.0.0', '1.8.7'];
 
-    this.array = versions.map((version) => ({
+    const arr = versions.map((version) => ({
       source: VersionSource.remote,
       state: VersionState.ready,
       version,
     }));
 
-    this.object = {};
-    for (const ver of this.array) {
-      this.object[ver.version] = ver;
+    const obj = {};
+    for (const ver of arr) {
+      obj[ver.version] = ver;
     }
 
-    this.mockVersions = this.object;
-    this.mockVersionsArray = this.array;
+    this.mockVersions = obj;
+    this.mockVersionsArray = arr;
   }
 }
-
-// export static instances for the tests that still use them
-export const { mockVersions, mockVersionsArray } = new MockVersions();
