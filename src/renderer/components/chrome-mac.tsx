8b9529904e80243b1880a8bb4de13b97279cebb6
Shelley Vohr <shelley.vohr@gmail.com>
2021-08-25 22:58:18 +0200

    Update src/renderer/components/chrome-mac.tsx
    Co-authored-by: Felix Rieseberg <felix@felixrieseberg.com>

--- src/renderer/components/chrome-mac.tsx
+++ src/renderer/components/chrome-mac.tsx
@@ -1,6 +1,5 @@
 import { observer } from 'mobx-react';
 import * as React from 'react';
-import * as fs from 'fs';
 import { IpcEvents } from '../../ipc-events';
 
 import { ipcRendererManager } from '../ipc';
@@ -15,21 +14,8 @@ interface ChromeMacProps {
  *
  * @returns Whether or not macOS is Big Sur or later.
  */
-const isBigSurOrLater = () => {
-  const file = '/System/Library/CoreServices/SystemVersion.plist';
-  if (!fs.existsSync(file)) return false;
-
-  const plist = fs.readFileSync(file, 'utf8');
-  const matches = /<key>ProductVersion<\/key>\s*<string>([\d.]+)<\/string>/.exec(
-    plist,
-  );
-
-  if (!matches) return false;
-
-  const version = matches[1].replace('10.16', '11');
-
-  return parseInt(version, 10) >= 11;
-};
+const isBigSurOrLater = () =>
+  Number(require('os').release().split('.')[0]) >= 20;
 
 @observer
 export class ChromeMac extends React.Component<ChromeMacProps> {
