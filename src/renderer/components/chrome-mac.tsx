f1cf91e7ef37967ed9d5e058e8f37df7557bb639
Shelley Vohr <shelley.vohr@gmail.com>
2021-08-25 23:12:44 +0200

    fix: title bar alignment on macOS (#722)

--- src/renderer/components/chrome-mac.tsx
+++ src/renderer/components/chrome-mac.tsx
@@ -9,6 +9,14 @@ interface ChromeMacProps {
   appState: AppState;
 }
 
+/**
+ * Helper method to determine if macOS is running Big Sur or later.
+ *
+ * @returns Whether or not macOS is Big Sur or later.
+ */
+const isBigSurOrLater = () =>
+  Number(require('os').release().split('.')[0]) >= 20;
+
 @observer
 export class ChromeMac extends React.Component<ChromeMacProps> {
   public handleDoubleClick = () => {
@@ -18,8 +26,13 @@ export class ChromeMac extends React.Component<ChromeMacProps> {
   public render() {
     if (process.platform !== 'darwin') return null;
 
+    let className = 'chrome drag';
+    if (isBigSurOrLater()) {
+      className += ' big_sur_or_later';
+    }
+
     return (
-      <div className="chrome drag" onDoubleClick={this.handleDoubleClick}>
+      <div className={className} onDoubleClick={this.handleDoubleClick}>
         <small>{this.props.appState.title}</small>
       </div>
     );
