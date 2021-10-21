e309cbe110b344bd1364085b69a01d23503df359
John Kleinschmidt <jkleinsc@electronjs.org>
2021-10-21 10:42:17 -0400

    fix: remove REACT_PERF extension

--- src/main/devtools.ts
+++ src/main/devtools.ts
@@ -12,15 +12,11 @@ export async function setupDevTools(): Promise<void> {
   const {
     default: installExtension,
     REACT_DEVELOPER_TOOLS,
-    REACT_PERF,
   } = require('electron-devtools-installer');
 
   try {
     const react = await installExtension(REACT_DEVELOPER_TOOLS);
     console.log(`installDevTools: Installed ${react}`);
-
-    const perf = await installExtension(REACT_PERF);
-    console.log(`installDevTools: Installed ${perf}`);
   } catch (error) {
     console.warn(`installDevTools: Error occured:`, error);
   }
