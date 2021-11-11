18359030a16e3b34820a865ba418ca81ffcbb4b0
Timothy Lim <tim.lim@intercom.io>
2021-11-11 22:23:08 +0800

    fix(show-me/crashreporter): refactor to use current Electron security defaults

--- static/show-me/crashreporter/main.js
+++ static/show-me/crashreporter/main.js
@@ -3,14 +3,24 @@
 // For more info, see:
 // https://electronjs.org/docs/api/crash-reporter
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, crashReporter } = require('electron')
+const path = require('path')
+
+crashReporter.start({
+  productName: 'YourName',
+  companyName: 'YourCompany',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  uploadToServer: true
+})
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: false, // default in Electron >= 5
+      contextIsolation: true, // default in Electron >= 12
+      preload: path.join(__dirname, 'preload.js')
     }
   })
   mainWindow.loadFile('index.html')
