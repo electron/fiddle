ebb0d5147dbc282a4c2117f0dcaa134177b5c6f8
Erick Zhao <erick@hotmail.ca>
2021-11-16 12:05:25 -0800

    fix(show-me): update clipboard and shell demos with preload (#903)
    Co-authored-by: Deepak Sreekumar <deepakstark@gmail.com>
    Co-authored-by: DEEPAK S <deepak.s@safe.security>

--- static/show-me/clipboard/main.js
+++ static/show-me/clipboard/main.js
@@ -4,13 +4,14 @@
 // https://electronjs.org/docs/api/clipboard
 
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 600,
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
