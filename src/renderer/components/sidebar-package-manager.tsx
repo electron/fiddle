baae94178bd32f6c9ce73c69f47a0f19bac5ac0d
Erick Zhao <erick@hotmail.ca>
2022-01-26 15:37:15 -0800

    initial impl

--- src/renderer/components/sidebar-package-manager.tsx
+++ src/renderer/components/sidebar-package-manager.tsx
@@ -1,14 +1,34 @@
 import * as React from 'react';
-import { Button, InputGroup, Tree, TreeNodeInfo } from '@blueprintjs/core';
-import { AppState } from '../state';
+import { Button, MenuItem, Tree, TreeNodeInfo } from '@blueprintjs/core';
+import { Suggest } from '@blueprintjs/select';
+import algoliasearch from 'algoliasearch/lite';
+import pDebounce from 'p-debounce';
 import { observer } from 'mobx-react';
 
+import { AppState } from '../state';
+
+const client = algoliasearch('OFCNCOG2CU', '4efa2042cf4dba11be6e96e5c394e1a4');
+const index = client.initIndex('npm-search');
+
 interface IState {
-  search: string;
+  suggestions: Array<AlgoliaHit>;
+  versionsCache: Map<string, string[]>;
 }
 
 interface IProps {
   appState: AppState;
+}
+
+// See full schema: https://github.com/algolia/npm-search#schema
+interface AlgoliaHit {
+  name: string;
+  version: string;
+  versions: Record<string, string>;
+  _highlightResult: {
+    name: {
+      value: string;
+    };
+  };
 }
 
 @observer
@@ -16,71 +36,117 @@ export class SidebarPackageManager extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      search: '',
+      suggestions: [],
+      versionsCache: new Map(),
     };
   }
 
-  public addPackageToSearch = (event: React.KeyboardEvent) => {
-    const { search } = this.state;
+  public addPackageToFiddle = (item: AlgoliaHit) => {
     const { appState } = this.props;
-    if (event.key === 'Enter' && !!search) {
-      // TODO: allow users to specify the package version
-      appState.modules.set(search, '*');
-      this.setState({ search: '' });
-    }
+    appState.modules.set(item.name, item.version);
+    // copy state so react can re-render
+    this.state.versionsCache.set(item.name, Object.keys(item.versions));
+    const newCache = new Map(this.state.versionsCache);
+    this.setState({ suggestions: [], versionsCache: newCache });
   };
 
   public render() {
+    console.log(this.state.suggestions);
     return (
-      <Tree
-        contents={[
-          {
-            childNodes: [
-              {
-                id: '-1',
-                label: (
-                  <InputGroup
-                    leftIcon="search"
-                    placeholder="Add package"
-                    fill
-                    onChange={(event) =>
-                      this.setState({ search: event.target.value })
-                    }
-                    onKeyPress={this.addPackageToSearch}
-                    value={this.state.search}
-                  />
-                ),
-              },
-              ...this.renderModules(),
-            ],
-            id: 'modules',
-            hasCaret: false,
-            icon: 'code-block',
-            label: 'Modules',
-            isExpanded: true,
-          },
-        ]}
-      />
+      <div>
+        <Suggest
+          fill={true}
+          initialContent={null}
+          inputValueRenderer={(item) => item.name}
+          items={this.state.suggestions}
+          itemRenderer={(item, { handleClick }) => (
+            <MenuItem
+              key={item.name}
+              text={
+                <span
+                  className="package-manager-result"
+                  dangerouslySetInnerHTML={{
+                    __html: item._highlightResult.name.value,
+                  }}
+                />
+              }
+              onClick={handleClick}
+            />
+          )}
+          onItemSelect={this.addPackageToFiddle}
+          onQueryChange={pDebounce(async (query) => {
+            const { hits } = await index.search<AlgoliaHit>(query);
+            this.setState({
+              suggestions: hits,
+            });
+          }, 200)}
+          popoverProps={{ minimal: true, usePortal: false }}
+        />
+        <Tree contents={this.getModuleNodes()} />
+      </div>
     );
   }
 
-  public renderModules = (): TreeNodeInfo[] => {
+  /**
+   * Takes the module map and returns an object
+   * conforming to the BlueprintJS tree schema.
+   * @returns TreeNodeInfo[]
+   */
+  public getModuleNodes = (): TreeNodeInfo[] => {
     const values: TreeNodeInfo[] = [];
     const { appState } = this.props;
-    for (const pkg of appState.modules.keys()) {
+    for (const [pkg, activeVersion] of appState.modules.entries()) {
       values.push({
         id: pkg,
         label: pkg,
         secondaryLabel: (
-          <Button
-            minimal
-            icon="remove"
-            onClick={() => appState.modules.delete(pkg)}
-          />
+          <div>
+            <select
+              name={pkg}
+              value={activeVersion}
+              onChange={({ target }) =>
+                appState.modules.set(target.name, target.value)
+              }
+            >
+              {this.state.versionsCache.get(pkg)?.map((version) => (
+                <option key={version}>{version}</option>
+              ))}
+            </select>
+
+            <Button
+              minimal
+              icon="remove"
+              onClick={() => appState.modules.delete(pkg)}
+            />
+          </div>
         ),
       });
     }
 
     return values;
+  };
+
+  /**
+   * Attempt to fetch the list of all versions for
+   * all installed modules.
+   */
+  public refreshVersionsCache = async () => {
+    const cache = this.props.appState.modules;
+    for (const pkg of cache.keys()) {
+      const { hits } = await index.search<AlgoliaHit>(pkg);
+      const firstMatch = hits.pop();
+      if (firstMatch === undefined || !firstMatch.versions) {
+        console.error(
+          `Attempted to fetch version list for ${pkg} from Algolia but failed!`,
+        );
+        return;
+      } else {
+        this.state.versionsCache.set(
+          firstMatch.name,
+          Object.keys(firstMatch.versions),
+        );
+        this.setState({ versionsCache: new Map(this.state.versionsCache) });
+      }
+    }
   };
 }
