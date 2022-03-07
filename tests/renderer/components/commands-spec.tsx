e9907ac0ef0c529d7f4befff224de313b4919a6e
Black-Hole1 <158blackhole@gmail.com>
2022-03-07 17:07:02 +0800

    test: improve commands component test

--- tests/renderer/components/commands-spec.tsx
+++ tests/renderer/components/commands-spec.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 
 import { Commands } from '../../../src/renderer/components/commands';
+import { BisectHandler } from '../../../src/renderer/components/commands-bisect';
+import { ipcRendererManager } from '../../../src/renderer/ipc';
+import { IpcEvents } from '../../../src/ipc-events';
 
 import { StateMock } from '../../mocks/mocks';
 
@@ -31,5 +34,24 @@ describe('Commands component', () => {
   it('renders', () => {
     const wrapper = shallow(<Commands appState={store as any} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('show bisect command', () => {
+    store.isBisectCommandShowing = true;
+    const wrapper = shallow(<Commands appState={store as any} />);
+
+    expect(wrapper.find(BisectHandler).length).toBe(1);
+  });
+
+  it('handleDoubleClick()', () => {
+    const spy = jest.spyOn(ipcRendererManager, 'send');
+
+    const wrapper = shallow(<Commands appState={store as any} />);
+    const instance = wrapper.instance() as any;
+
+    instance.handleDoubleClick();
+
+    expect(spy).toHaveBeenCalledWith(IpcEvents.CLICK_TITLEBAR_MAC);
+    spy.mockRestore();
   });
 });
