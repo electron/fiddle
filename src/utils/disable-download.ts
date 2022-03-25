bb77ca24ae6d3a9b4c85d39f595d9ef9a620e596
Ayman161803 <mohammedaymannawaz@gmail.com>
2022-03-25 16:53:09 +0530

    Added JSDoc to disable-downloads.ts

--- src/utils/disable-download.ts
+++ src/utils/disable-download.ts
@@ -1,6 +1,14 @@
 import semver from 'semver';
 import { isJest } from './is-jest';
 
+/**
+ * disables download button for versions below 11.0.0 on Apple Silicon.
+ * Reference: {@link https://www.electronjs.org/blog/apple-silicon}
+ *
+ * @param {string} version
+ * @returns {boolean}
+ */
+
 export function disableDownload(version: string) {
   return (
     !isJest() &&
