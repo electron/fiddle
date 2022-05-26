6a0fd04407ccf5266f756750ec5dc96f5fac511a
Shelley Vohr <shelley.vohr@gmail.com>
2022-05-26 21:33:51 +0200

    fix: react devtools not loading

--- src/main/devtools.ts
+++ src/main/devtools.ts
@@ -15,7 +15,9 @@ export async function setupDevTools(): Promise<void> {
   } = require('electron-devtools-installer');
 
   try {
-    const react = await installExtension(REACT_DEVELOPER_TOOLS);
+    const react = await installExtension(REACT_DEVELOPER_TOOLS, {
+      loadExtensionOptions: { allowFileAccess: true },
+    });
     console.log(`installDevTools: Installed ${react}`);
   } catch (error) {
     console.warn(`installDevTools: Error occurred:`, error);
