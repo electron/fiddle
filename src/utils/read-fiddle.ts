4372da5da6ff2ea4446b5edb1011fc64859b1086
David Sanders <dsanders11@ucsbalum.com>
2022-06-26 01:36:21 -0700

    fix: work around bug in readdir in graceful-fs

--- src/utils/read-fiddle.ts
+++ src/utils/read-fiddle.ts
@@ -18,7 +18,9 @@ export async function readFiddle(
   let got: EditorValues = {};
 
   try {
-    const files = await fs.readdir(folder);
+    // TODO(dsanders11): Remove empty options once issue fixed:
+    // https://github.com/isaacs/node-graceful-fs/issues/223
+    const files = await fs.readdir(folder, {});
     const names = files.filter(
       (f) => isSupportedFile(f) || (includePackageJson && f === PACKAGE_NAME),
     );
