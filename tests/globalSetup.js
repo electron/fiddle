c8d9560bac417d0b36bc0d5cc1d94a2c8ac652e3
David Sanders <dsanders11@ucsbalum.com>
2023-04-14 01:09:49 -0700

    build: simplify contributors.json generation (#1312)

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,30 +1,7 @@
-const path = require('path');
+const { maybeFetchContributors } = require('../tools/contributors');
 
-const fs = require('fs-extra');
-
-const CONTRIBUTORS_FILE_PATH = path.join(
-  __dirname,
-  '../static/contributors.json',
-);
-
-async function maybePopulateEmptyContributors() {
-  try {
-    await fs.readJson(CONTRIBUTORS_FILE_PATH);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      // File does not exist, create an empty JSON file instead
-      await populateEmptyContributors();
-    } else if (error) {
-      throw error;
-    }
-  }
+async function globalSetup() {
+  await maybeFetchContributors(true);
 }
 
-/**
- * Creates an empty JSON file at CONTRIBUTORS_FILE_PATH location
- */
-async function populateEmptyContributors() {
-  await fs.outputFile(CONTRIBUTORS_FILE_PATH, JSON.stringify([]));
-}
-
-module.exports = maybePopulateEmptyContributors;
+module.exports = globalSetup;
