389caebd4b55c702191a66aeff879672e15c8404
David Sanders <dsanders11@ucsbalum.com>
2023-04-25 23:20:48 -0700

    build: use @electron/fiddle-core to fetch releases

--- tools/fetch-releases.js
+++ tools/fetch-releases.js
@@ -1,30 +1,20 @@
 const path = require('path');
 
-const fetch = require('cross-fetch');
+const { ElectronVersions } = require('@electron/fiddle-core');
 const fs = require('fs-extra');
 
 const file = path.join(__dirname, '..', 'static', 'releases.json');
 
-async function getReleases() {
-  const url = 'https://releases.electronjs.org/releases.json';
-  const response = await fetch(url, {
-    headers: {
-      'User-Agent': 'Electron Fiddle',
-    },
-  });
-  return await response.json();
-}
-
 async function populateReleases() {
-  const data = await getReleases();
-  const releases = data.map(({ version, node }) => ({ version, node }));
+  const elves = await ElectronVersions.create(undefined, { ignoreCache: true });
+  const releases = elves.versions.map(({ version }) =>
+    elves.getReleaseInfo(version),
+  );
 
   console.log(`Updating local releases.json with ${releases.length} versions.`);
 
   await fs.remove(file);
-  await fs.outputFile(file, JSON.stringify(releases));
-
-  console.log('Updating tests with new expected version count.');
+  await fs.outputJSON(file, releases);
 }
 
 module.exports = {
