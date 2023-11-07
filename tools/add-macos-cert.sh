d9238a8d69388c003d0527a5186959494e25cb66
Felix Rieseberg <fr@makenotion.com>
2023-11-07 08:41:35 -0800

    Quick fix: Correctly escape and ingest certificate

--- tools/add-macos-cert.sh
+++ tools/add-macos-cert.sh
@@ -4,9 +4,11 @@ KEY_CHAIN=build.keychain
 MACOS_CERT_P12_FILE=certificate.p12
 
 # Recreate the certificate from the secure environment variable
-echo $MACOS_CERT_P12 | base64 --decode > $MACOS_CERT_P12_FILE
+echo "$MACOS_CERT_P12" | base64 -d > "$MACOS_CERT_P12_FILE"
+file_size=$(stat -f%z "$MACOS_CERT_P12_FILE")
+echo "Certificate size is $file_size bytes"
 
-#create a keychain
+# Create a keychain
 security create-keychain -p actions $KEY_CHAIN
 
 # Make the keychain the default so identities are found
@@ -15,7 +17,7 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P $MACOS_CERT_PASSWORD -T /usr/bin/codesign;
+security import $MACOS_CERT_P12_FILE -k $KEY_CHAIN -P "$MACOS_CERT_PASSWORD" -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
