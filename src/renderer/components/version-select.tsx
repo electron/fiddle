af906094b5a32e690111acb487f8d2b76ea35c5c
Erik Moura <erikian@electronjs.org>
2024-01-16 23:19:44 -0300

    test: use RTL in the version selector tests (#1435)
    * test: use RTL in the version selector tests

--- src/renderer/components/version-select.tsx
+++ src/renderer/components/version-select.tsx
@@ -205,6 +205,7 @@ export const renderItem: ItemRenderer<RunnableVersion> = (
       >
         <MenuItem
           active={modifiers.active}
+          data-testid="disabled-menu-item"
           disabled={true}
           text={highlightText(item.version, query)}
           key={item.version}
