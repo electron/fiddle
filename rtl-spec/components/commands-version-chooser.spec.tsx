0d0e844b15c543fe0081eeeaab12aedbacfb0369
David Sanders <dsanders11@ucsbalum.com>
2024-12-06 15:17:29 -0800

    build: clear lint warnings regarding test import

--- rtl-spec/components/commands-version-chooser.spec.tsx
+++ rtl-spec/components/commands-version-chooser.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 
 import { VersionChooser } from '../../src/renderer/components/commands-version-chooser';
 import { AppState } from '../../src/renderer/state';
--- rtl-spec/components/version-select.spec.tsx
+++ rtl-spec/components/version-select.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 import { mocked } from 'jest-mock';
 
 import {
