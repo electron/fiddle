53057ba0fd63b52c94f7aed9b60c810ec55cf92c
Shelley Vohr <shelley.vohr@gmail.com>
2025-04-08 22:39:46 +0200

    feat: allow packaging a local electron version (#1702)

--- src/renderer/transforms/forge.ts
+++ src/renderer/transforms/forge.ts
@@ -1,11 +1,14 @@
-import { Files, PACKAGE_NAME } from '../../interfaces';
+import { Files, PACKAGE_NAME, RunnableVersion } from '../../interfaces';
 import { getForgeVersion } from '../utils/get-package';
 
 /**
  * This transform turns the files into an electron-forge
  * project.
  */
-export async function forgeTransform(files: Files): Promise<Files> {
+export async function forgeTransform(
+  files: Files,
+  version?: RunnableVersion,
+): Promise<Files> {
   if (files.get(PACKAGE_NAME)) {
     try {
       const parsed = JSON.parse(files.get(PACKAGE_NAME)!);
@@ -45,6 +48,26 @@ export async function forgeTransform(files: Files): Promise<Files> {
         config.forge.electronRebuildConfig = {
           forceABI: parseInt(modules.toString().trim()),
         };
+      }
+
+      // Package local version if available.
+      if (version?.localPath) {
+        devDependencies['@electron-forge/plugin-local-electron'] = forgeVersion;
+        config.forge.plugins = [
+          {
+            name: '@electron-forge/plugin-local-electron',
+            config: {
+              electronPath: version.localPath,
+            },
+          },
+        ];
+
+        // Replace electron dep in package.json with a legitimate
+        // released version so it doesn't error on npm install.
+        if (parsed.devDependencies.electron) {
+          const latest = await window.ElectronFiddle.getLatestStable();
+          parsed.devDependencies.electron = latest?.version;
+        }
       }
 
       // electron-forge makers
